5d28567be6106ec8d7f5f5e9270ebab907048777
Naoweiei <koranitnoonrod@gmail.com>
2026-08-21 14:40:40 +0700

    fix template

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,7 +3,7 @@
     <q-form class="q-gutter-md" style="max-width: 600px">
       <q-input filled label="Your name *" hint="Name and surname" />
       <q-input filled type="number" label="Your age *" />
-      <q-toggle label="I accept the license and terms (我接受许可和条款语言)" />
+      <q-toggle label="I accept the license and terms" />
       <div>
         <q-btn label="SUBMIT" type="submit" color="primary" />
         <q-btn
